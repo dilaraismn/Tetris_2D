022021784148c97a059f5cabf35e5ef554047588
Dilara <dilaraismn@gmail.com>
2023-01-19 16:39:18 +0300

    Movement 01

--- Assets/_Scripts/Board.cs
+++ Assets/_Scripts/Board.cs
@@ -10,7 +10,17 @@ public class Board : MonoBehaviour
     public Tilemap tilemap { get; private set; }
     public TetrominoData[] tetrominoes;
     public Piece activePiece { get; private set; }
-    public Vector3Int spawnPosition;
+    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds 
+    { 
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -44,4 +54,33 @@ public class Board : MonoBehaviour
             this.tilemap.SetTile(tilePosition, piece.data.tile);
         }
     }
+    
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition)) {
+                return false;
+            }
+
+            if (tilemap.HasTile(tilePosition)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
