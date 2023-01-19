1f7ed867307c95723c51ecf3030eb3e3cbbabea3
Dilara <dilaraismn@gmail.com>
2023-01-19 15:14:48 +0300

    Setting Up Data

--- Assets/_Scripts/Tetromino.cs
+++ Assets/_Scripts/Tetromino.cs
@@ -1,16 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Tetromino : MonoBehaviour
+public enum Tetromino
 {
-    void Start()
-    {
-        
-    }
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z,
+}
 
-    void Update()
+[System.Serializable]
+public struct TetrominoData
+{
+    public Tetromino tetromino;
+    public Tile tile;
+    public Vector2Int[] cells { get; private set; }
+
+    public void Initialize()
     {
-        
+        this.cells = Data.Cells[this.tetromino];
     }
 }
